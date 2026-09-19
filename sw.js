01ce7300ffd0b3c0244401c7d479b85fcd6e0e18
hanserikroset-web <hanserikroset@gmail.com>
2026-09-19 08:22:41 +0200

    Add blood and insemination type registrations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='brunstreg-v1';
+const CACHE='brunstreg-v2';
 const FILES=['./','index.html','styles.css','modal.css','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
