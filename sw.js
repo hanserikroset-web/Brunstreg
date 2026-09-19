1753f73713dbbfbb1fa5f9e761d51a4d01cb3f98
hanserikroset-web <hanserikroset@gmail.com>
2026-09-19 07:08:05 +0200

    Fix modal layout and offline assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='brunstreg-v1';
-const FILES=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
+const FILES=['./','index.html','styles.css','modal.css','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
